95d4253e032dd964a3a4249dce6e3cb08431390f
Adam Wood <woodadam272@gmail.com>
2024-04-28 14:14:25 -0600

    documented strands.h

--- cpp/strands.h
+++ cpp/strands.h
@@ -1,7 +1,9 @@
+/** **********************************************************************
+* @file
+* @brief contains all function prototypes and structures
+*************************************************************************/
 #ifndef _STRANDS_H_
 #define _STRANDS_H_
-
-#define __WEBASM__
 
 #include <iostream>
 #include <vector>
@@ -30,8 +32,16 @@ struct PuzzleWord;
 struct LetterNode;
 
 #define LETTER_NODE_MAX 27
+
+/** **********************************************************************
+*  @brief
+*  Node in a prefix tree.
+************************************************************************/
 struct LetterNode {
+
 	LetterNode * children [ LETTER_NODE_MAX ];
+    /**< Children nodes. One for 26 letters of the alphabet plus one extra */
+
 	LetterNode();
 	~LetterNode();
 	bool contains( string word );
@@ -40,13 +50,26 @@ struct LetterNode {
 	LetterNode * & operator []( char c );
 };
 
+
+/** **********************************************************************
+*  @brief
+*  Represents a single word on the Strands board.
+************************************************************************/
 struct PuzzleWord {
 	
 	string word;
+    /**< The word on the board. */
 	int ** coordinates;
+    /**< A 2d array of the board representing the position of 
+	each letter in the word on the board with the first letter being marked 1
+	the second letter being marked 2, etc, and every position that isn't in 
+	the word marked 0. */
+
 
 	inline static int width;
+    /**< Width of the Strands board. */
 	inline static int height;
+    /**< Height of the strands board. */
 
 	PuzzleWord();
 	PuzzleWord( PuzzleWord & other );
@@ -75,6 +98,12 @@ struct PuzzleWord {
 
 bool operator < ( const PuzzleWord & lhs, const PuzzleWord & rhs );
 
+/** **********************************************************************
+*  @brief
+*  Represents the Strands board. Contains the text, words found on the board,
+*  possible solutions to the puzzle, and more. This is the class that does most
+*  of the work to solve and help solve the puzzle.
+************************************************************************/
 class StrandsBoard {
 	
 	public:
@@ -96,20 +125,35 @@ class StrandsBoard {
 		bool in_bounds( int x, int y );
 
 		char ** board;
+    	/**< Text of the strands board. */
 
 		bool ** used;
+    	/**< 2d tracking what letters have been used already. */
 
 		bool ** hint_coors;
+    	/**< 2d array representing which letters are in a hint */
 
 		LetterNode prefix_tree;
+    	/**< Prefix tree of all english words with a length of at least 4 and
+		no non-letters. */
 
 		set<string> dictionary;
+    	/**< Dictionary of all english words with a length of at least 4 and 
+		no non-letters. */
+
 
 		vector<PuzzleWord> found_words;
+    	/**< Vector of all words on the board that don't overlap with the 
+		used letters. */
+
 
 		vector<vector<int>> solutions;
+    	/**< Vector of vectors of ints representing a list of indecies to words
+		that solutions to the puzzle. */
 
 		vector<PuzzleWord> possible_hints;
+    	/**< Vector of words that match a hint. */
+
 
 		void print_solutions( ostream & out = std::cout );
 		
@@ -147,17 +191,26 @@ class StrandsBoard {
 	private: 
 
 		int width;
+    	/**< Width of the Strands board. */
+
 		int height;
+    	/**< Height of the Strands board. */
 
 		string json_holder;
+    	/**< JSON representation of a solution for interacting with js */
 
 		char ** words;
+    	/**< List of words in a solution. Not used. */
 
 		int words_len;
-
-		int max_words = 9;
+    	/**< Number of words in the words array. Not used */
 
 		int curr_word_len;
+    	/**< Length of the last word in the working solution. Not used.  */
+
+		int max_words = 9;
+    	/**< Maximum number of words in a solution */
+
 
 		void find_all_words_from_point( char * word_str, int ** coors, set<string> & already_found, int x, int y, int word_len ); 
 
